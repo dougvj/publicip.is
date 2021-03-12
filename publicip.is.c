7b08a35326515e42bfe0d4fa057b39c2b3b84076
Doug Johnson <dougvj@gmail.com>
2021-03-12 05:20:16 -0700

    Preliminary epoll implementation TODO timeout support, fix edge level trigger for accept

--- publicip.is.c
+++ publicip.is.c
@@ -1,3 +1,5 @@
+#include <asm-generic/socket.h>
+#define _GNU_SOURCE
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -13,15 +15,66 @@
 #include <stdint.h>
 #include <stdatomic.h>
 #include <signal.h>
+#include <sys/epoll.h>
+
+#define MAX_BUF_LEN 1024
+
 
 struct client_data {
+  int epoll;
   int client_sock;
+  int num_end_headers_bytes_found;
   struct sockaddr_in6 client_addr;
+  char buffer[MAX_BUF_LEN];
+  int buf_len;
+  char* buf_ptr;
+  int thread_num;
 };
+
+
+void init_client_data(struct client_data* d, int epoll) {
+  d->epoll = epoll;
+  d->buf_len = MAX_BUF_LEN;
+  d->buf_ptr = d->buffer;
+  d->num_end_headers_bytes_found = 0;
+}
+
+bool client_cont_read(struct client_data* d) {
+  struct epoll_event ev;
+  ev.data.fd = d->client_sock;
+  ev.data.ptr = d;
+  ev.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
+  if (epoll_ctl(d->epoll, EPOLL_CTL_MOD, d->client_sock, &ev) == -1) {
+    perror("epoll_ctl: EPOLL_CTL_MOD: client_sock");
+    return false;
+  }
+  return true;
+}
+
+bool client_cont_write(struct client_data* d) {
+  struct epoll_event ev;
+  ev.data.fd = d->client_sock;
+  ev.data.ptr = d;
+  ev.events = EPOLLOUT | EPOLLET | EPOLLONESHOT;
+  if (epoll_ctl(d->epoll, EPOLL_CTL_MOD, d->client_sock, &ev) == -1) {
+    perror("epoll_ctl: EPOLL_CTL_MOD: client_sock");
+    return false;
+  }
+  return true;
+}
+
+#define MAX_EVENTS 1
+
+struct app_context {
+  int port;
+  int num_threads;
+  int sock;
+  int epoll;
+  pthread_t* threads;
+} context;
 
 #define eprintf(...) fprintf(stderr, __VA_ARGS__);
 
-#define MAX_BUF_LEN 1024
 
 void _500(int client) {
   char error_response[] = "HTTP/1.0 500 Internal Server Error\r\n\r\n";
@@ -30,55 +83,43 @@ void _500(int client) {
   };
 }
 
-bool get_start_line_and_headers(int client, char* buffer, int max_buf_len) {
+bool get_start_line_and_headers(struct client_data* d) {
   // Look for end of headers
   static const char END_HEADERS_BYTES[] = "\r\n\r\n";
-  int num_end_headers_bytes_found = 0;
-  for (;;) {
-    int num = recv(client, buffer, max_buf_len, 0);
-    if (num == -1) {
-      if (errno == EAGAIN) {
-        eprintf("recv: Socket timed out\n");
+  int num = recv(d->client_sock, d->buf_ptr, d->buf_len, 0);
+  if (num == -1) {
+    if (errno == EAGAIN) {
+      return client_cont_read(d);
+    }
+    perror("recv");
+    return false;
+  }
+  if (num == 0) {
+    eprintf("recv: Unexpected socket close\n");
+    return false;
+  }
+  for (int i = 0; i < num; i++) {
+    if (d->buf_ptr[i] == END_HEADERS_BYTES[d->num_end_headers_bytes_found]) {
+      d->num_end_headers_bytes_found++;
+      if (d->num_end_headers_bytes_found == sizeof(END_HEADERS_BYTES) - 1) {
+        return client_cont_write(d);
       }
-      perror("recv");
-      return false;
-    }
-    if (num == 0) {
-      eprintf("recv: Unexpected socket close\n");
-      return false;
-    }
-    for (int i = 0; i < num; i++) {
-      if (buffer[i] == END_HEADERS_BYTES[num_end_headers_bytes_found]) {
-        num_end_headers_bytes_found++;
-        if (num_end_headers_bytes_found == sizeof(END_HEADERS_BYTES) - 1) {
-          return true;
-        }
-      } else {
-        num_end_headers_bytes_found = 0;
-      }
-    }
-    buffer += num;
-    max_buf_len -= num;
-    if (max_buf_len == 0) {
-      eprintf("recv: Buffer Full\n");
-      return false;
+    } else {
+      d->num_end_headers_bytes_found = 0;
     }
   }
+  d->buf_ptr += num;
+  d->buf_len -= num;
+  if (d->buf_len == 0) {
+    eprintf("recv: Buffer Full\n");
+    return false;
+  }
+  return client_cont_read(d);
 }
 
 
 
-void return_ip(struct client_data* d, int thread_num) {
-  int client = d->client_sock;
-  // Set socket timeout to 5
-  struct timeval tv = {
-    .tv_sec = 5
-  };
-  setsockopt(client, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
-  char buffer[MAX_BUF_LEN];
-  if (get_start_line_and_headers(client, buffer, MAX_BUF_LEN) == false) {
-    goto fail;
-  }
+bool return_ip(struct client_data* d) {
   // TODO parse the start line in case there are wierd requests
   char ip[INET6_ADDRSTRLEN];
   struct in6_addr* addr6 = &(d->client_addr.sin6_addr);
@@ -98,11 +139,11 @@ void return_ip(struct client_data* d, int thread_num) {
   if (inet_ntop(family, addr, ip, INET6_ADDRSTRLEN) == NULL) {
     perror("inet_ntop");
     _500(d->client_sock);
-    goto fail;
+    return false;
   };
   unsigned int ip_len = strlen(ip);
   int response_len = snprintf(
-      buffer,
+      d->buffer,
       MAX_BUF_LEN,
       "HTTP/1.0 200 OK\r\n"
       "Content-Length: %u\r\n"
@@ -112,54 +153,101 @@ void return_ip(struct client_data* d, int thread_num) {
       ip_len + 1,
       ip
   );
-  if (send(d->client_sock, buffer, response_len, 0) == -1) {
-      perror("send");
+  if (send(d->client_sock, d->buffer, response_len, 0) == -1) {
+    if (errno == EAGAIN) {
+      return client_cont_write(d);
+    }
+    perror("send");
+    return false;
   }
-  eprintf("%3i: %s\n", thread_num, ip);
-fail:
-  shutdown(d->client_sock, SHUT_RDWR);
-  close(d->client_sock);
+  eprintf("%3i: %s\n", d->thread_num, ip);
+  return false;
+}
+
+void process_client(struct epoll_event ev) {
+  struct client_data *d = ev.data.ptr;
+  bool cont = false;
+  if (ev.events & EPOLLIN) {
+    cont = get_start_line_and_headers(d);
+  }
+  if (ev.events & EPOLLOUT) {
+    cont = return_ip(d);
+  }
+  if (!cont) {
+    epoll_ctl(d->epoll, EPOLL_CTL_MOD, d->client_sock, NULL);
+    shutdown(d->client_sock, SHUT_RDWR);
+    close(d->client_sock);
+    free(d);
+  }
 }
 
 void* worker_thread(void* data) {
-  int sock = ((intptr_t)(data));
+  struct app_context* context = data;
   static atomic_int thread_count = 0;
   int thread_num = atomic_fetch_add(&thread_count, 1);
   eprintf("Thread %i starting\n", thread_num);
-  struct client_data d;
-  socklen_t client_sock_len = sizeof(d.client_addr);
+  struct epoll_event events[MAX_EVENTS];
   for (;;) {
-    int client =
-        accept(sock, (struct sockaddr *)&(d.client_addr), &client_sock_len);
-    if (client == -1) {
-      // Socket was closed
-      if (errno == EBADF || errno == EINVAL) {
+    int nfds = epoll_wait(context->epoll, events, MAX_EVENTS, -1);
+    if (nfds == -1) {
+      perror("epoll_wait");
+      if (errno == EINTR) {
         break;
       }
-      perror("accept");
-      continue;
     }
-    d.client_sock = client;
-    return_ip(&d, thread_num);
+    for (int n = 0; n < nfds; n++) {
+      if (events[n].data.fd == context->sock) {
+        for(;;) {
+          struct client_data* d = malloc(sizeof(struct client_data));
+          socklen_t addrlen = sizeof(d->client_addr);
+          d->client_sock =
+              accept4(context->sock, (struct sockaddr *)&(d->client_addr),
+                      &addrlen, SOCK_NONBLOCK);
+          if (d->client_sock == -1) {
+            free(d);
+            if (errno != EAGAIN) {
+              perror("accept");
+            }
+            break;
+          }
+          init_client_data(d, context->epoll);
+          struct epoll_event ev;
+          ev.data.fd = d->client_sock;
+          ev.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
+          ev.data.ptr = d;
+          if (epoll_ctl(context->epoll, EPOLL_CTL_ADD, d->client_sock, &ev) ==
+              -1) {
+            perror("epoll_ctl: EPOLL_CTL_ADD: conn_sock");
+            close(d->client_sock);
+            free(d);
+            continue;
+          }
+        }
+      } else {
+        struct client_data* d = events[n].data.ptr;
+        d->thread_num = thread_num;
+        process_client(events[n]);
+      }
+    }
   }
   eprintf("Thread %i stopping\n", thread_num);
   return 0;
 }
 
-struct app_context {
-  int port;
-  int num_threads;
-  int sock;
-  pthread_t* threads;
-} context;
 
 void handle_term(int signo) {
-  int num_threads = context.num_threads;
-  eprintf("\nStopping server...\n");
-  shutdown(context.sock, SHUT_RDWR);
-  close(context.sock);
-  for (int i = 0; i < num_threads; i++) {
-    pthread_join(context.threads[i], NULL);
+  if (signo == SIGTERM || signo == SIGINT) {
+    int num_threads = context.num_threads;
+    eprintf("\nStopping server...\n");
+    for (int i = 0; i < num_threads; i++) {
+      pthread_kill(context.threads[i], SIGUSR1);
+    }
+    for (int i = 0; i < num_threads; i++) {
+      pthread_join(context.threads[i], NULL);
+    }
+    shutdown(context.sock, SHUT_RDWR);
+    close(context.sock);
+    close(context.epoll);
   }
 }
 
@@ -188,7 +276,7 @@ int main(int argc, char** argv) {
         num_threads = get_nprocs();
       }
     }
-    int sock = socket(AF_INET6, SOCK_STREAM, 0);
+    int sock = socket(AF_INET6, SOCK_STREAM | SOCK_NONBLOCK, 0);
     if (sock == -1) {
         perror("socket");
         return EXIT_FAILURE;
@@ -205,22 +293,38 @@ int main(int argc, char** argv) {
       perror("bind");
       exit(EXIT_FAILURE);
     }
-    if (listen(sock, 128) < 0) {
+    if (listen(sock, 1) < 0) {
       perror("listen");
       exit(EXIT_FAILURE);
     }
-    // Block signals in threads (signal masks are inherited)
+    context.epoll = epoll_create1(0);
+    if (context.epoll == -1) {
+      perror("epoll_create1");
+      exit(EXIT_FAILURE);
+    }
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = sock;
+    if (epoll_ctl(context.epoll, EPOLL_CTL_ADD, sock, &ev) == -1) {
+      perror("epoll_ctl: listen_sock");
+      exit(EXIT_FAILURE);
+    }
+
+    // Block signals in threads except SIGUSR1 (signal masks are inherited)
     sigset_t mask;
     sigfillset(&mask);
+    sigdelset(&mask, SIGUSR1);
     pthread_sigmask(SIG_SETMASK, &mask, NULL);
+    signal(SIGUSR1, handle_term);
     // Create threads
     pthread_t threads[num_threads];
     for (int i = 0; i < num_threads; i++) {
-      pthread_create(&(threads[i]), NULL, worker_thread,
-                     (void *)((intptr_t)sock));
+      pthread_create(&(threads[i]), NULL, worker_thread, &context);
     }
-    // Unblock signals
+    // Unblock signals except SIGUSR1, so SIGUSR1 goes to threads and the rest
+    // goes to main thread
     sigemptyset(&mask);
+    sigaddset(&mask, SIGUSR1);
     pthread_sigmask(SIG_SETMASK, &mask, NULL);
     signal(SIGTERM, handle_term);
     signal(SIGINT, handle_term);
